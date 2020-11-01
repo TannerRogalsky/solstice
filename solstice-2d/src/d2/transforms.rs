1fc6a4790728fad2bd3efd15981edc510a7720ad
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-11-01 06:46:17 -0500

    transform component creators and accumulation operations

--- solstice-2d/src/d2/transforms.rs
+++ solstice-2d/src/d2/transforms.rs
@@ -22,11 +22,62 @@ impl Default for Transform {
 }
 
 impl Transform {
+    pub fn rotation<R: Into<super::Rad>>(phi: R) -> Self {
+        let phi = phi.into();
+        let (sin, cos) = phi.0.sin_cos();
+        Self {
+            scale_x: cos,
+            skew_y: -sin,
+            skew_x: sin,
+            scale_y: cos,
+            ..Default::default()
+        }
+    }
+
+    pub fn scale(scale: f32) -> Self {
+        Self {
+            scale_x: scale,
+            scale_y: scale,
+            ..Default::default()
+        }
+    }
+
+    pub fn translation(x: f32, y: f32) -> Self {
+        Self {
+            translation_x: x,
+            translation_y: y,
+            ..Default::default()
+        }
+    }
+
     pub fn transform_point(&self, x: f32, y: f32) -> (f32, f32) {
         (
             x * self.scale_x + y * self.skew_x + self.translation_x,
             x * self.skew_y + y * self.scale_y + self.translation_y,
         )
+    }
+}
+
+impl std::ops::Mul for Transform {
+    type Output = Self;
+
+    fn mul(mut self, rhs: Self) -> Self::Output {
+        let lhs = self;
+        self.scale_x = lhs.scale_x * rhs.scale_x + lhs.skew_x * rhs.skew_y;
+        self.skew_y = lhs.skew_y * rhs.scale_x + lhs.scale_y * rhs.skew_y;
+        self.skew_x = lhs.scale_x * rhs.skew_x + lhs.skew_x * rhs.scale_y;
+        self.scale_y = lhs.skew_y * rhs.skew_x + lhs.scale_y * rhs.scale_y;
+        self.translation_x =
+            lhs.scale_x * rhs.translation_x + lhs.skew_x * rhs.translation_y + lhs.translation_x;
+        self.translation_y =
+            lhs.skew_y * rhs.translation_x + lhs.scale_y * rhs.translation_y + lhs.translation_y;
+        self
+    }
+}
+
+impl std::ops::MulAssign for Transform {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
     }
 }
 
@@ -135,6 +186,46 @@ mod tests {
 
         let (px, py) = (0., 0.);
         assert_eq!(identity.transform_point(px, py), (2., 1.5));
+    }
+
+    #[test]
+    pub fn transform_point_rotation() {
+        use approx::*;
+
+        let transform = Transform::rotation(crate::Deg(90.));
+        assert_abs_diff_eq!(transform.scale_x, 0.);
+        assert_abs_diff_eq!(transform.scale_y, 0.);
+        assert_abs_diff_eq!(transform.skew_x, 1.);
+        assert_abs_diff_eq!(transform.skew_y, -1.);
+
+        let (px, py) = (0., 0.);
+        assert_eq!(transform.transform_point(px, py), (px, py));
+
+        let (px, py) = (1., 0.);
+        let (tx, ty) = transform.transform_point(px, py);
+        assert_abs_diff_eq!(tx, 0.);
+        assert_abs_diff_eq!(ty, -1.);
+
+        let (tx, ty) = transform.transform_point(tx, ty);
+        assert_abs_diff_eq!(tx, -px);
+        assert_abs_diff_eq!(ty, -py);
+
+        let (px, py) = (2., 2.);
+        let (tx, ty) = transform.transform_point(px, py);
+        assert_abs_diff_eq!(tx, 2., epsilon = 0.001);
+        assert_abs_diff_eq!(ty, -2., epsilon = 0.001);
+
+        let (tx, ty) = transform.transform_point(tx, ty);
+        assert_abs_diff_eq!(tx, -2., epsilon = 0.001);
+        assert_abs_diff_eq!(ty, -2., epsilon = 0.001);
+
+        let (tx, ty) = transform.transform_point(tx, ty);
+        assert_abs_diff_eq!(tx, -2., epsilon = 0.001);
+        assert_abs_diff_eq!(ty, 2., epsilon = 0.001);
+
+        let (tx, ty) = transform.transform_point(tx, ty);
+        assert_abs_diff_eq!(tx, 2., epsilon = 0.001);
+        assert_abs_diff_eq!(ty, 2., epsilon = 0.001);
     }
 
     #[test]
